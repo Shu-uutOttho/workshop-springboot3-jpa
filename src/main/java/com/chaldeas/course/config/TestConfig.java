836055c0d6ffec5b3cc1c4a8084e151f04289f11
Shu <controvictor@gmail.com>
2022-10-16 19:44:41 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/chaldeas/course/config/TestConfig.java
+++ src/main/java/com/chaldeas/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.chaldeas.course.entities.Category;
 import com.chaldeas.course.entities.Order;
+import com.chaldeas.course.entities.OrderItem;
 import com.chaldeas.course.entities.Product;
 import com.chaldeas.course.entities.User;
 import com.chaldeas.course.entities.enums.OrderStatus;
 import com.chaldeas.course.repositories.CategoryRepository;
+import com.chaldeas.course.repositories.OrderItemRepository;
 import com.chaldeas.course.repositories.OrderRepository;
 import com.chaldeas.course.repositories.ProductRepository;
 import com.chaldeas.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -72,5 +77,11 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	}
 }
