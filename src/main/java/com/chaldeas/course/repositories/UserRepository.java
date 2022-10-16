4182dbfa7f798b4028e7cca40f117635341ba29c
Shu <controvictor@gmail.com>
2022-10-16 14:37:02 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/chaldeas/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.chaldeas.course.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chaldeas.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
